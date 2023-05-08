bb380138a2fabff7d6f04960a942ffffd5b43f5d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 07:23:05 -0600

    update

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- links -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/css/bootstrap.min.css">
+
     <title>Monitoring</title>
 </head>
 <body>
@@ -15,5 +19,7 @@
             <a href="/alerts.php">Alerts</a>
         </li>
     </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
