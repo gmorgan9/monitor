a6736dbdc7e025d41a3122d3458e41c7747c9d9d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 07:54:11 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -7,7 +7,7 @@ $result = mysqli_query($conn, $select);
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
     $fname = $row['firstname'];
-// }}
+}}
 ?>
 
 
@@ -29,7 +29,7 @@ if (mysqli_num_rows($result) > 0) {
         </a>
 
         <ul class="navbar-nav ms-auto d-flex flex-row">
-            <?php if(isset($row['fname'])){ ?>
+            <?php if(isset($_SESSION['fname'])){ ?>
             <?php //if($row['acc_type'] == 1){ ?>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0">Welcome, <span style="text-transform: capitalize;"><?php echo $fname; ?></span>!</a></li>
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="/">Home</a></li>
@@ -42,9 +42,6 @@ if (mysqli_num_rows($result) > 0) {
                 <li class="nav-item"><a class="nav-link me-3 me-lg-0" href="<?php echo BASE_URL . '/core/entry/login.php' ?>">Login/Signup</a></li>
             <?php } ?>
         </ul>
-        <?php
-    }}
-?>
     </div>
 <!-- Container wrapper -->
 </nav>
