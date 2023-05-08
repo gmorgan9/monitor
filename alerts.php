e3348854b042cbf98dcfc339b93f7a7c175cb93b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 16:51:38 -0600

    update

--- alerts.php
+++ alerts.php
@@ -16,6 +16,7 @@ session_start();
 
     <!-- links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <title>Alerts</title>
 </head>
@@ -26,7 +27,7 @@ session_start();
     <!-- container -->
         <div class="container">
 
-            <button id="refreshButton" class="btn btn-primary mt-3"><a class="text-decoration-none text-white" href="<?php $_SERVER['PHP_SELF']; ?>">Refresh</a></button>
+            <a class="text-decoration-none text-secondary" href="<?php $_SERVER['PHP_SELF']; ?>"><i class="bi bi-arrow-clockwise"></i></a>
 
             <table class="table table-bordered mt-2">
                 <thead>
