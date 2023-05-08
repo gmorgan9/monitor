213051e471d38b378da0f193e99611e85c4067ee
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 16:44:43 -0600

    update

--- alerts.php
+++ alerts.php
@@ -16,6 +16,9 @@ session_start();
 
     <!-- links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 
     <title>Alerts</title>
 </head>
