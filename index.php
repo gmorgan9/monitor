a0b924e94bb241904f759ca6a2d1788fba9be960
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 07:27:30 -0600

    update

--- index.php
+++ index.php
@@ -13,12 +13,11 @@
 <body>
 <?php include("app/includes/header.php"); ?>
 
-    <h1>Alert Monitoring</h1>
-    <nav>
-        <li>
-            <a href="/alerts.php">Alerts</a>
-        </li>
-    </nav>
+    <div class="container text-start">
+
+        <a href="/alerts.php">Alerts</a>
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
