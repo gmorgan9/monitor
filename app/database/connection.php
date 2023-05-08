b621772afcdb65147c62a4c881ea83c788821fdf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 07:32:12 -0600

    update

---
+++ app/database/connection.php
@@ -0,0 +1,19 @@
+<!-- WORKING -->
+<?php
+date_default_timezone_set('America/Denver');
+
+
+$servername = "127.0.0.1";
+$username = "DBadmin";
+$password = "DBadmin123!";
+$database = "monitor";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if ($conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+echo "happy";
+} 
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once "app/database/connection.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
