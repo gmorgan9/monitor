c57b71f4b1bbf52b07d985a3817aff52b94559dc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 12:23:44 -0600

    update

--- core/sensors/index.php
+++ core/sensors/index.php
@@ -35,7 +35,7 @@ if (!isLoggedIn()) {
     <!-- container -->
         <div class="container">
 
-            <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" style="background-color: pink;" class="btn btn-primary float-end mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button>
 
