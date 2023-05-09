06d4b30f350fab6881924a7718814e5238de20d1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 23:57:32 -0600

    update

--- core/admin/index.php
+++ core/admin/index.php
@@ -33,60 +33,62 @@ if (!isAdmin()) {
 <?php include("../../app/includes/header.php"); ?>
 
 <div class="container">
-        <h1>Welcome to the Admin Page</h1>
-        <p>This is the protected area for admin users only.</p>
+    <h1>Welcome to the Admin Page</h1>
+    <p>This is the protected area for admin users only.</p>
 
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>User ID</th>
-                    <th>Name</th>
-                    <th>Username</th>
-                    <th>Account</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Account</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
             <?php
-                $sql = "SELECT * FROM users";
-                $all = mysqli_query($conn, $sql);
-                if($all) {
-                    while ($row = mysqli_fetch_assoc($all)) {
-                        $idno     = $row['idno'];
-                        $email       = $row['email'];
-                        $uname       = $row['username'];
-                        $fname       = $row['firstname'];
-                        $lname       = $row['lastname'];
-                        $admin       = $row['isadmin'];
-                        $status      = $row['status'];
-                    }
-            ?>
-                
+            $sql = "SELECT * FROM users";
+            $all = mysqli_query($conn, $sql);
+            if ($all) {
+                while ($row = mysqli_fetch_assoc($all)) {
+                    $idno     = $row['idno'];
+                    $email    = $row['email'];
+                    $uname    = $row['username'];
+                    $fname    = $row['firstname'];
+                    $lname    = $row['lastname'];
+                    $admin    = $row['isadmin'];
+                    $status   = $row['status'];
+                    ?>
 
                     <tr>
                         <td><?php echo $idno; ?></td>
                         <td><?php echo $lname; ?>, <?php echo $fname; ?></td>
                         <td><?php echo $uname; ?></td>
 
-                        <?php if($admin == 1) { ?>
+                        <?php if ($admin == 1) { ?>
                             <td>Admin</td>
                         <?php } else { ?>
                             <td>Standard</td>
                         <?php } ?>
 
-                        <?php if($status == 'approved') { ?>
+                        <?php if ($status == 'approved') { ?>
                             <td class="text-success">Approved</td>
-                        <?php } else if($status == 'pending') { ?>
+                        <?php } else if ($status == 'pending') { ?>
                             <td class="text-primary">Pending</td>
                         <?php } else { ?>
                             <td class="text-danger">Declined</td>
-                        <?php }?>
+                        <?php } ?>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+
+            <?php
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
