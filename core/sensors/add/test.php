40ec38e539a5ebaba05e3c51a0ed8e9c3ad53832
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 13:19:40 -0600

    update

--- core/sensors/add/test.php
+++ core/sensors/add/test.php
@@ -34,7 +34,7 @@ if (!isLoggedIn()) {
 
     <div class="container">
 
-        <form action="" method="post" class="w-75 mx-auto">
+        <form action="" method="post" class="w-75 d-flex justify-content-center">
 
             <div class="row">
                 <div class="col-md-6">
