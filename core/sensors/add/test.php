de8f4cd48971911783be1ddbdbcbd5a16ee0bd8e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 13:25:03 -0600

    update

--- core/sensors/add/test.php
+++ core/sensors/add/test.php
@@ -59,9 +59,9 @@ if (!isLoggedIn()) {
                     <input type="text" class="form-control" id="ip_addr" placeholder="Enter IP address">
                 </div>
                 <div class="mb-3">
-                    <label for="sensor_type" class="">Sensor Type</label>
-                    <select class="" id="sensor_type">
-                        <option value="" disabled selected style="color: #6c757d;">Select Sensor Type</option>
+                    <label for="sensor_type" class="form-label">Sensor Type</label>
+                    <select class="form-control" id="sensor_type">
+                        <option value="" disabled style="color: #6c757d;">Select Sensor Type</option>
                         <option value="option1">Option 1</option>
                         <option value="option2">Option 2</option>
                         <option value="option3">Option 3</option>
