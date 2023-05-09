e52a9f6021429267434420bb40d410ea5c6ca209
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 12:53:16 -0600

    update

--- core/sensors/add/index.php
+++ core/sensors/add/index.php
@@ -1,15 +1,9 @@
 <?php
 
+
 // require_once '../../../app/database/connection.php';
-// require_once '../../../app/database/functions.php';
-// require_once "../../../path.php";
-// require_once ROOT_PATH . "/app/database/connection.php";
-// require_once ROOT_PATH . "/app/database/functions.php";
-// include(ROOT_PATH . "path.php");
-require_once '../../../app/database/connection.php';
-// require_once '../../../app/database/functions.php';
-require_once '../../../path.php';
-//require_once ROOT_PATH . "/execute.php";
+require_once '../../../app/database/functions.php';
+// require_once '../../../path.php';
 
 
 session_start();
