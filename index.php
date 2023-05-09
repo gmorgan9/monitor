649c526fadc780785039e9e817c15bb2b2eed12a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 00:21:21 -0600

    update

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
-
-require_once "app/database/connection.php";
-require_once "app/database/functions.php";
 require_once "path.php";
-session_start();
-
-if ($_SERVER['HTTPS'] != 'on') {
-    // Redirect to HTTPS
-    $url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $url");
-    exit();
-}
-
+header('Location: ' . BASE_URL . '/core/entry/login.php');
+exit;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,22 +10,10 @@ if ($_SERVER['HTTPS'] != 'on') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- links -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    <title>Dashboard</title>
 </head>
 <body>
-<?php include("app/includes/header.php"); ?>
 
-    <div class="container text-start">
-
-        <h1>Dashboard</h1>
-
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<h1>main</h1>
 </body>
 </html>
