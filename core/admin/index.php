3a8d608a6ed3f42b72111d2e75cc501d0f54e676
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 23:39:48 -0600

    update

--- core/admin/index.php
+++ core/admin/index.php
@@ -27,14 +27,15 @@ if (!isAdmin()) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    <title>Monitoring</title>
+    <title>LogDetect | Admin</title>
 </head>
 <body>
 <?php include("../../app/includes/header.php"); ?>
 
     <div class="container text-start">
 
-        <a href="/alerts.php">Alerts</a>
+        <h1>Welcome to the Admin Page</h1>
+        <p>This is the protected area for admin users only.</p>
 
     </div>
 
