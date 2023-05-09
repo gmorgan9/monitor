f2f8430ce33ccd954708ba540ed3beff26d4fa63
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-08 23:04:59 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -4,5 +4,5 @@ session_start();
 require('connection.php');
 
 function isLoggedIn() {
-    return isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true;
+    return isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 1;
 }
