90eb5a55ed63efb444412cd9d0416a67ccb04d06
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 15:36:07 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-// require_once '../../path.php';
-include("/path.php");
+require_once '../../path.php';
+// include("/path.php");
 require('connection.php');
 
 function isLoggedIn() {
