cfc08c5084fbad423caa56b9af8d15c1a3c7c7b1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 00:08:37 -0600

    update

--- app/database/functions.php
+++ app/database/functions.php
@@ -23,31 +23,36 @@ function isAdmin() {
     	$isadmin = $_POST['isadmin'];
     	$loggedin = $_POST['loggedin'];
     
-    	$select = " SELECT * FROM users WHERE username = '$uname' && password = '$pass' && status = 'approved' ";
-    
-    	$result = mysqli_query($conn, $select);
-    
-    	if(mysqli_num_rows($result) > 0){
-        
-    	   $row = mysqli_fetch_array($result);
-    	   $sql = "UPDATE users SET loggedin='1' WHERE username='$uname'";
-    	   mysqli_query($conn, $sql);
-    		$_SESSION['fname']           = $row['firstname'];
-    		$_SESSION['uid']             = $row['user_id'];
-    		$_SESSION['loggedin']        = $row['loggedin'];
-    		$_SESSION['idno']            = $row['idno'];
-    		$_SESSION['lname']           = $row['lastname'];
-    		$_SESSION['uname']           = $row['username'];
-    	    $_SESSION['email']           = $row['email'];
-    	    $_SESSION['pass']            = $row['password'];
-            $_SESSION['admin']           = $row['isadmin'];
-    	    header('location:' . BASE_URL . '/core/alerts/');
-        
-    	}else{
-    	   $error[] = 'Possible Incorrect email or password! Account could not be approved yet!';
-    	}
-    
-    };
+    	$select = "SELECT * FROM users WHERE username = '$uname' && password = '$pass'";
+
+        $result = mysqli_query($conn, $select);
+
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_array($result);
+
+            if ($row['status'] === 'pending') {
+                $error[] = 'Your account is pending approval.';
+            } else {
+                $sql = "UPDATE users SET loggedin='1' WHERE username='$uname'";
+                mysqli_query($conn, $sql);
+
+                $_SESSION['fname'] = $row['firstname'];
+                $_SESSION['uid'] = $row['user_id'];
+                $_SESSION['loggedin'] = $row['loggedin'];
+                $_SESSION['idno'] = $row['idno'];
+                $_SESSION['lname'] = $row['lastname'];
+                $_SESSION['uname'] = $row['username'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['pass'] = $row['password'];
+                $_SESSION['admin'] = $row['isadmin'];
+
+                header('Location: ' . BASE_URL . '/core/alerts/');
+                exit;
+            }
+        } else {
+            $error[] = 'Incorrect email or password!';
+        }
+    }
 // end LOGIN
 
 
