790ece9780bceeaea0b249e8f70e6149be7e8962
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 13:04:02 -0600

    update

--- core/sensors/test.php
+++ core/sensors/test.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "../../app/database/connection.php";
-require_once "../../app/database/functions.php";
+// require_once "../../app/database/functions.php";
 require_once "../../path.php";
 require_once "../../execute.php";
 
