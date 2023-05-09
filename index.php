3a19a97a8893934ac915b229cfbce6a89527ad87
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-05-09 00:22:05 -0600

    update

--- index.php
+++ index.php
@@ -9,11 +9,8 @@ exit;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
 </head>
 <body>
-
-<h1>main</h1>
+<h1>redirecting to login page</h1>
 </body>
 </html>
